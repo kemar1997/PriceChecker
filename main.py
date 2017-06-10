20f4fde89c9ba7aa1e881efae07b389ed0b12348
kemar1997 <kemar.1997@outlook.com>
2017-06-10 03:31:33 -0400

    gonna reimplement fncionality with another site

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
 
+# div = item_name.get('div')
+
 # Tickets to be searched throughout the different online vendors
-def concert_tickets(item_url):
+# Defining the initial product
+def concert_tickets():
 	url = "https://www.stubhub.com/florida-georgia-line-tickets-florida-georgia-line-wantagh-northwell-health-at-jones-beach-theater-6-15-2017/event/9806266/?mbox=1&rS=6&abbyo=true&sliderpos=false&qtyq=false&qtyddab=true&sort=price+asc"
-	source_code = requests.get(item_url)
+	source_code = requests.get(url)
 	plain_text = source_code.text
 	soup = BeautifulSoup(plain_text, "html.parser")
-	for item_name in soup.findAll('div', {'class': 'dollar-value'}):
-		div = item_name.get('div')
-		print(item_name.string)
-
+	for price_info in soup.findAll('div', {'class': 'partials'}):
+		div = prince_info.get('div')
+		#dollar-value = price_info.string
+		partials = price_info.string
+		#print(partials)
+		print(soup)
+		print(price_info)
 # Call the function for testing
-concert_tickets(div)
+concert_tickets()
 
